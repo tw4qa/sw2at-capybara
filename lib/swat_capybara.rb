0d5f528cc967065e082d7c732c4c9fd1483d28c0
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2014-11-21 19:26:41 +0200

    CL: remove unneded dependencies

--- lib/swat_capybara.rb
+++ lib/swat_capybara.rb
@@ -1,10 +1,5 @@
 module Swat
   module Capybara
-    require 'swat/capybara/methods'
-    require 'swat/capybara/templates'
-    require 'swat/capybara/data_generator'
-    require 'swat/capybara/situations'
-    require 'swat/capybara/setup'
     require 'swat/capybara/helpers'
   end
 end
