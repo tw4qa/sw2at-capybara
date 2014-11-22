1a5ee5046edcda73b560b3756039369379ffaa24
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2014-11-22 14:42:14 +0200

    -/-

--- lib/swat/capybara/setup.rb
+++ lib/swat/capybara/setup.rb
@@ -1,14 +1,10 @@
 module Swat
-  module Capybara
-    module Setup
-
-      def setup(config)
-        Capybara::Node::Base.include Swat::Capybara::Helpers
-        Capybara::Node::Simple.include Swat::Capybara::Helpers
-        Capybara::Session.include Swat::Capybara::Helpers
-        config.include Swat::Capybara::Helpers, type: :feature
-      end
-
+  module Setup
+    def setup(config)
+      ::Capybara::Node::Base.include Swat::Capybara::Helpers
+      ::Capybara::Node::Simple.include Swat::Capybara::Helpers
+      ::Capybara::Session.include Swat::Capybara::Helpers
+      config.include Swat::Capybara::Helpers, type: :feature
     end
   end
 end
